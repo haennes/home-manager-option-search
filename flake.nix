790a6feae762914d9db58c0d15c19166baaba0be
haennes <hannes.hofmuth@gmail.com>
2025-02-16 18:04:42 +0100

    fix: nix fmt

--- flake.nix
+++ flake.nix
@@ -5,22 +5,25 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    { self, nixpkgs, ... }:
     let
       forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
       pkgsForSystem = system: (import nixpkgs { inherit system; });
     in
     {
-      packages = forAllSystems (system:
+      packages = forAllSystems (
+        system:
         let
           pkgs = (pkgsForSystem system);
         in
         {
-          default = pkgs.runCommand "public" {} ''
+          default = pkgs.runCommand "public" { } ''
             cd ${./.}
             ${pkgs.hugo}/bin/hugo --noBuildLock -d $out
           '';
-        });
+        }
+      );
 
       devShells = forAllSystems (system: {
         default = (pkgsForSystem system).mkShell {
@@ -30,6 +33,7 @@
           ];
         };
       });
+
       formatter = forAllSystems (
         system:
         let
